8055be2185723ed185046b6c0683f0b7c28f36cd
Rohith M <rohithmerala@gmail.com>
2022-04-30 16:05:12 +0530

    V1

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+popup = document.getElementById("popup");
+
 Webcam.set({
   width: 350,
   height: 300,
@@ -12,7 +14,7 @@ function take_snapshot() {
   Webcam.snap(function (data_uri) {
     document.getElementById("result").innerHTML = '<img src="' + data_uri + '" id="captured_img"><br>';
   });
-  document.getElementById("taken_label").innerHTML = "<br>Result:";
+  document.getElementById("taken_label").innerHTML = "<br>Captured Image:";
   document.getElementById("identify_button").innerHTML = "<button class='btn btn-outline-primary' onclick='check()'>Identify Image</button><br><br>";
 }
 
@@ -22,4 +24,26 @@ classifier = ml5.imageClassifier("https://teachablemachine.withgoogle.com/models
 
 function modelloaded() {
   console.log("Successfully Loaded TeachableMachine API");
+}
+
+function check() {
+  img = document.getElementById("captured_img");
+  classifier.classify(img, gotresult);
+  popup.classList.add("open-popup");
+}
+
+function gotresult(error, results) {
+  if (error) {
+    console.error(error);
+  } else {
+    console.log(results);
+    final_accuracy = results[0].confidence.toFixed(2);
+    final_accuracy = (final_accuracy * 100).toFixed(2) + "%";
+    document.getElementById("result_object_name").innerHTML = results[0].label;
+    document.getElementById("result_object_accuracy").innerHTML = final_accuracy;
+  }
+}
+
+function closePopup() {
+  popup.classList.remove("open-popup");
 }
